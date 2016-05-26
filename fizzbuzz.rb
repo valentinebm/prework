35d3950f92389d36a4e16a9f753a2443cbcefb40
valentinebm <valentine.barbierm@gmail.com>
2016-05-26 21:36:37 +0200

    Add files via upload

---
+++ fizzbuzz.rb
@@ -0,0 +1,27 @@
+
+array = Array (1..100)
+
+for x in array
+	result = ""
+	if x % 3 == 0
+	result << "Fizz"
+	end 
+
+	if x % 5 == 0
+	result << "Buzz"
+	end 
+
+	if x.to_s[0] = "1"
+	result << "Bang"
+	end 
+
+	if result == ""
+	puts x
+	else 
+	puts result
+	end 
+end 
+
+
+
+
