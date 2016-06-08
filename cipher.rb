8d6683da94a2c45d33e330f9da4c671c3ec88d6a
valentinebm <valentine.barbierm@gmail.com>
2016-06-08 15:27:14 +0200

    Add files via upload

--- cipher.rb
+++ cipher.rb
@@ -1,11 +1,20 @@
-def solve_cipher(input)
-	array_ascii = input.split(//).map {|l| l.ord}
-	array_shifted = array_ascii.map {|l| l - 1}
-	puts array_shifted.map {|l| l.chr}.join
-end
+def solve_cipher(input, shift=-3)
+	array_ascii = input.split(//).map  do 
+		|l| l.ord 
+	end
+	array_shifted = array_ascii.map  do 
+		|l| l + shift
+	end
+	array_corrected = array_shifted.map do |l| 
+		(l<97 && l>93 || l<65 && l>61 ? l + 26 : l) 
+	
+	end 
+
+	puts array_corrected.map {|l| l.chr}.join
+end 
 
 
-puts "What message do you want to decipher?"
-message = gets.chomp
-solve_cipher(message)
-
+solve_cipher("abcdefghijklmnopqrstuvwxyz")
+solve_cipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+solve_cipher("p| uhdo qdph lv grqdog gxfn")
+#still need to solve the space issue, gogo
